2abd362e524d9e01243431996714923589a47833
rustamNSU <54931369+rustamNSU@users.noreply.github.com>
2022-02-01 15:11:13 +0700

    added pkgs installing test

---
+++ check_installed_pkgs.py
@@ -0,0 +1,11 @@
+import matplotlib
+print('matplotlib version = {}'.format(matplotlib.__version__))
+
+import scipy
+print('scipy version = {}'.format(scipy.__version__))
+
+import PySide6
+print('PySide6 version = {}'.format(PySide6.__version__))
+
+import pandas
+print('pandas version = {}'.format(pandas.__version__))
