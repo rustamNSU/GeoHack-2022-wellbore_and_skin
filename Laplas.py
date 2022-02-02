b6e3204fc179fee54eebbd0c66b993140f4e8706
Abdullin Rustam <afaritovich@mail.ru>
2022-02-02 17:43:17 +0700

    fix laplace_pwd (return to math.sqrt)

--- Laplas.py
+++ Laplas.py
@@ -1,6 +1,6 @@
 import math
 import numpy as np
-from scipy.special import kv, expi
+from scipy.special import kv, k0, k1, expi
 import matplotlib.pyplot as plt
 import mpmath as mp
 
@@ -12,17 +12,19 @@ import mpmath as mp
 def exact_solution(t, r):
     return -expi(-r^2/(4*t))
 
-def laplace_p_wd(Skin, CD, s): 
-    pwd = ( (kv(0, s**0.5) + s**0.5*Skin*kv(1, s**0.5))/(s**0.5*kv(1, s**0.5) + CD*s*(kv(0,s**0.5)+s**0.5*Skin*kv(1, s**0.5))) )/s
+def laplace_p_wd(Skin, CDstor, s): 
+    pwd = ((k0(math.sqrt(s)) + math.sqrt(s)*Skin*k1(math.sqrt(s)))/(math.sqrt(s)*k1(math.sqrt(s)) + CDstor*s*(k0(math.sqrt(s))+math.sqrt(s)*Skin*k1(math.sqrt(s)))))/s
     return pwd 
     
 
 s = np.linspace(0, 2, 100)  
-fp = lambda p: laplace_p_wd(0, 0, p)
+
+
+fp = lambda p: laplace_p_wd(0.0, 0.0, p)
 
 tt = [1.0, 10.0, 100.0]
-# print([mp.invertlaplace(fp, t, method='stehfest') for t in tt])
-print(mp.invertlaplace(fp, 1, method='stehfest'))
+print([mp.invertlaplace(fp, t, method='stehfest') for t in tt])
+# print(mp.invertlaplace(fp, 1.0, method='stehfest'))
 
 # fig, ax = plt.subplots()                        
 
