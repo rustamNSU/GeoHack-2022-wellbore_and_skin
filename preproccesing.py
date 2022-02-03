f568f53e66366e45e693cb753bbf070be4defde4
IldarSorokin <ildar7816@yandex.ru>
2022-02-03 09:20:59 +0700

    Создание массивов забойного давления и времени

--- preproccesing.py
+++ preproccesing.py
@@ -1,19 +1,33 @@
+from datetime import datetime
+import numpy as np
 import pandas as pd
 
-data = pd.ExcelFile("data/GDIS.xlsx")
+def read_field_data_xlsx(file_path):
+    data = pd.ExcelFile(file_path)
 
-data.sheet_names
+    table = data.parse('1')
 
-pressure = pd.read_excel(data, '1')
+    pressure = table['Данные'].to_numpy()
 
-pressure = pressure[['Date', 'Данные']]
+    index = [0]
+    new_pressure = np.delete(pressure, index)
+    pressure = new_pressure / 10.197
 
-pressure = pressure.drop(index=0)
+    timedata = table['Date'].to_numpy()
+    
 
-pressure['Данные'] = pressure['Данные']/10.197
+    index = [0]
+    new_timedata = np.delete(timedata, index)
+    new_time_date = []
 
-from datetime import timedelta
-for i in range(31262):
-    print(timedelta(seconds=i * 3))
+    start_time = new_timedata[0]
+    for date in new_timedata:
+        seconds = int(str(np.timedelta64(date - start_time, 's')).split(' ')[0])
+        new_time_date.append(seconds)
+    return pressure, new_time_date
 
-print(pressure)
+if __name__ == "__main__":
+
+    P, T = read_field_data_xlsx('data/GDIS.xlsx')
+
+    print("pressure = {}, time = {}".format(P[0], T[0]))
