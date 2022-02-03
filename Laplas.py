3e54236eceb5ff7904d82431846c0b743446dee9
Aigul13 <yapparovaaigul87@gmail.com>
2022-02-03 09:30:54 +0700

    + test

--- Laplas.py
+++ Laplas.py
@@ -8,23 +8,24 @@ import mpmath as mp
 def exact_solution(t, r):
     return -0.5*expi( -r**2 / (4*t) )
 
-def laplace_p_wd(Skin, CDstor, s): 
+def laplace_p_wd(Skin, CDstor, s):
+    s = float(s)
     pwd = (k0(math.sqrt(s)) + math.sqrt(s)*Skin*k1(math.sqrt(s))) / ( ( math.sqrt(s)*k1(math.sqrt(s) ) + CDstor*s*( k0(math.sqrt(s)) + math.sqrt(s)*Skin*k1(math.sqrt(s)) ) ) * s)
     return pwd 
     
 
+if __name__ == "__main__":
+    fp = lambda p: laplace_p_wd(0, 0, p)
 
-fp = lambda p: laplace_p_wd(0.0, 0.0, p)
+    tt = np.linspace(1, 1000, 50)
 
-tt = np.linspace(1, 1000, 50)
+    fig, ax = plt.subplots()   
 
-fig, ax = plt.subplots()   
+    ax.plot(tt, [mp.invertlaplace(fp, t, method='stehfest', degree = 20) for t in tt], color="blue")
+    ax.plot(tt, [exact_solution(t, 1.0) for t in tt], color="red")
+    ax.set_xscale('log') 
+    # ax.set_xlabel("x")                              # подпись у горизонтальной оси х
+    # ax.set_ylabel("y")                              # подпись у вертикальной оси y
+    ax.legend()                                     # показывать условные обозначения
 
-ax.plot(tt, [mp.invertlaplace(fp, t, method='stehfest', degree = 20) for t in tt], color="blue")
-ax.plot(tt, [exact_solution(t, 1.0) for t in tt], color="red")
-# ax.set_yscale('log') 
-# ax.set_xlabel("x")                              # подпись у горизонтальной оси х
-# ax.set_ylabel("y")                              # подпись у вертикальной оси y
-ax.legend()                                     # показывать условные обозначения
-
-plt.show()                                
+    plt.show()                                
