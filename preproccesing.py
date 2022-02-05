b21b65a97c6225df1c04d651854412c852e27133
IldarSorokin <ildar7816@yandex.ru>
2022-02-05 11:48:40 +0700

    Отрисовали функцию зависимости дебита от времени

--- preproccesing.py
+++ preproccesing.py
@@ -1,82 +1,89 @@
 from dataclasses import field
 from datetime import datetime
+from matplotlib.pyplot import plot
 import numpy as np
 import pandas as pd
-# import schedule 
-# import time
+import time
 
-# def definite_pressure():
-#     print("q")
+class FieldData: 
+    def __init__(self):
+        self.pwf = []
+        self.pTime = []
+        self.q = []
+        self.qTime = []
+        self.rw = 0.108
+        self.h = 43.8
+        self.phi = 0.06
+        self.B = 1.24
+        self.mu = 1.64
+        self.ct = 0.000225
+        self.pi = 13386077.25
 
-# schedule.every().seconds.at("t").do(definite_pressure)
-# schedule.every()
+    def print_data(self, n=10):
+        print("pressure = {}, time_in_seconds = {}, schedule_in_seconds = {}, debit = {}".format(\
+        self.pwf[0:n], self.pTime[0:n], self.qTime[0:n], self.q[0:n]))
 
-# class FieldData: 
-#     def __init__(self):
-#         # self.pwf = 
-#         # self.pTime = 
-#         # self.q = 
-#         # self.qTime = 
-#         self.rw = 0.108
-#         self.h = 43.8
-#         self.phi = 0.06
-#         self.B = 1.24
-#         self.mu = 1.64
-#         self.ct = 0.000225
-#         self.pi = 13386077.25
+    def get_debit(self, t):
+        indx = -1
+        for qt in self.qTime:
+            if t < qt:
+                break
+            else:
+                indx = indx + 1
+        return self.q[indx]
 
 
 def read_field_data_xlsx(file_path):
-
-    # field_data = FieldData()
+    field_data = FieldData()
 
     data = pd.ExcelFile(file_path)
-
     table = data.parse('1')
 
     pressure = table['pressure'].to_numpy()
 
-    index = [0]
-    new_pressure = np.delete(pressure, index)
-    pressure = new_pressure * 98066.5
+    pressure = pressure[1:]
+    pressure = pressure * 98066.5
 
     timedata = table['date'].to_numpy()
+    timedata = timedata[1:]
 
-    index = [0]
-    new_timedata = np.delete(timedata, index)
-    new_time_date = []
-
-    start_time = new_timedata[0]
-    for date in new_timedata:
+    time_in_seconds = np.zeros(len(timedata))
+    start_time = timedata[0]
+    for i, date in zip(range(len(timedata)), timedata):
         seconds = int(str(np.timedelta64(date - start_time, 's')).split(' ')[0])
-        new_time_date.append(seconds)
+        time_in_seconds[i] = seconds
 
     schedule = table['schedule'].to_numpy()
     removeNan = ~np.isnan(schedule)
     schedule = schedule[removeNan]
 
-    # index = [0]
-    # new_schedule = np.delete(schedule, index)
-    new_schedule = schedule
-    new_time_date2 = []
+    schedule_in_seconds = np.zeros(len(schedule))
 
-    # start_time = new_schedule[0]
-    for date in new_schedule:
+
+    for i, date in zip(range(len(schedule)), schedule):
         seconds = int(str(np.timedelta64(date-start_time, 's')).split(' ')[0])
-        new_time_date2.append(seconds)
+        schedule_in_seconds[i] = seconds
 
-    expenditure = table['q'].to_numpy()
+    debit = table['q'].to_numpy()
+    debit = debit[removeNan] / 86400.0
 
-    # index = [0]
-    # new_expenditure = np.delete(expenditure, index)
-    expenditure = expenditure[removeNan] / 86400
+    field_data.pwf = pressure
+    field_data.pTime = time_in_seconds
+    field_data.q = debit
+    field_data.qTime = schedule_in_seconds
 
-
-    return pressure, new_time_date, new_time_date2, expenditure
-
+    return field_data
 
 if __name__ == "__main__":
 
-    P, T, S, E = read_field_data_xlsx('data/GDIS.xlsx')
+    field_data = read_field_data_xlsx('data/GDIS.xlsx')
+    field_data.print_data()
 
-    print("pressure = {}, time = {}, schedule = {}, expenditure = {}".format(P[0:10], T[0:10], S[0:10], E[-10:-1]))
+    import matplotlib.pyplot as plt 
+
+    x = field_data.pTime
+
+    y = np.array(list(map(field_data.get_debit, x)))
+
+    plt.plot(x, y)
+    plt.show()
